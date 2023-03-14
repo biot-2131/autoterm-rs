c9f1a757a5929f7eeb53f6ce558e8e61e6af0189
biot-2131 <121760096+biot-2131@users.noreply.github.com>
2023-03-14 08:58:54 +0000

    Waldo

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,32 @@ mod print_screen;
 use crate::print_screen::print_screen;
 
 mod opencv_module;
-use crate::opencv_module::hello_world;
+use crate::opencv_module::*;
+
+type CvImage = opencv::prelude::Mat;
+type CvMat = opencv::core::Mat;
 
 fn main() {
 
-    let filename: String = print_screen(); 
-    println!("filename = {}", filename);
+    // let image_path: String = print_screen(); 
+    // println!("image_path = {}", image_path);
 
-    hello_world();
+    let templ_path = String::from("assets/terminal_top_left.png");
+    println!("templ_path = {}", templ_path);
+    let templ = load_image_path(&templ_path);
+    println!("templ = {:?}", templ);
+    // show_image(&templ);
+    let templ_gray = convert_to_gray(&templ);
+    // show_image(&templ_gray);
 
-    let image_filename = String::from("target/2023-03-13_11:15:40.png");
-    load_color_image(image_filename);
-    //let templ_filename = String::from("target/2023-03-13_11:15:40.png");
+    let image_path = String::from("assets/desktop.png");
+    println!("image_path = {}", image_path);
+    let image = load_image_path(&image_path);
+    println!("image = {:?}", image);
+    // show_image(&image);
+    let image_gray = convert_to_gray(&image);
+    // show_image(&image_gray);
+
+    find_waldo();
 
 }
