4fe71e41ce520990e6c3c7847c2889dda3664a9b
biot-2131 <121760096+biot-2131@users.noreply.github.com>
2023-03-14 06:27:33 +0000

    March_13

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,18 @@
 mod print_screen;
 use crate::print_screen::print_screen;
 
+mod opencv_module;
+use crate::opencv_module::hello_world;
+
 fn main() {
 
     let filename: String = print_screen(); 
     println!("filename = {}", filename);
+
+    hello_world();
+
+    let image_filename = String::from("target/2023-03-13_11:15:40.png");
+    load_color_image(image_filename);
+    //let templ_filename = String::from("target/2023-03-13_11:15:40.png");
 
 }
